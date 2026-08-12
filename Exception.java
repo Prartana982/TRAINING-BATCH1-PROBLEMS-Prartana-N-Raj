e601f081bd8a910481cb841f4b967c8deac955d6
Prartana <prartananraj2006@gmail.com>
2026-08-12 14:44:27 +0530

    Exceptions concepts

---
+++ Exception.java
@@ -0,0 +1,27 @@
+package training;
+import java.util.*;
+public class Exception {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr= new int[3];
+        String c= null;
+        try {
+            System.out.print(a / b+"\n");
+            System.out.print(arr[1]+"\n");
+            System.out.print(c);
+        } catch (ArithmeticException e) {
+            System.out.print("Division by 0 is not possible\n");
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            System.out.print("Array Index exceeded bounds\n");
+        }
+        catch(NullPointerException e)
+        {
+            System.out.print("No value is assigned to this\n");
+        }
+        System.out.print("Code is Executed\n");
+    }
+}
