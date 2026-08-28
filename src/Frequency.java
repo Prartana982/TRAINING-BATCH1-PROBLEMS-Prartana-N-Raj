7dc16654b569e4cb3c06f15f025da5ef668ee5a6
Prartana <prartananraj2006@gmail.com>
2026-08-28 11:26:57 +0530

    Map task frequency

---
+++ src/Frequency.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Frequency {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string in lowercase: ");
+        String k=sc.nextLine();
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<k.length();i++)
+        {
+            char ki = k.charAt(i);
+            char p =Character.toLowerCase(ki);
+            map.put(p,map.getOrDefault(p,0)+1);
+        }
+        for(char l:map.keySet())
+        {
+            System.out.print(l+" -> "+map.get(l)+"\n");
+        }
+
+    }
+}
