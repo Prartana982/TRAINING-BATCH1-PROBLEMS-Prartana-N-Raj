3e2e67bb60529834781e68d0539c466c7b86a8a6
Prartana <prartananraj2006@gmail.com>
2026-08-28 14:15:48 +0530

    Thread methods

--- src/Multithreadingdemo.java
+++ src/Multithreadingdemo.java
@@ -2,6 +2,10 @@ import java.util.*;
 class process extends Thread {
     public void run() {
         for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(5000);
+            }
+            catch(Exception e){}
             System.out.print("Thread body: " + i + "\n");
         }
     }
@@ -10,6 +14,9 @@ class process1 extends Thread{
     public void run()
     {
         for(int i=0;i<5;i++) {
+            try {
+                Thread.sleep(3000);
+            }catch(Exception e){};
             System.out.print("New process thread " + i + "\n");
         }
     }
@@ -19,6 +26,10 @@ class process1 extends Thread{
     {
         process p1 = new process();
         process1 p2 = new process1();
+        p1.setName("New thread 1");
+        p2.setName("New thread 2");
+        System.out.print(p1.isAlive());
+        System.out.print(p2.isAlive());
         p1.start();
         p2.start();
         for(int i=0;i<5;i++)
